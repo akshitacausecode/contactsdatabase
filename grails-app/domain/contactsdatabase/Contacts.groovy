92834985e74e1103b417f69acf89deacabad7a53
akshita gangrade <akshitagangrade2@gmail.com>
2016-09-23 08:30:24 +0530

    Constraints added in domain class.

--- grails-app/domain/contactsdatabase/Contacts.groovy
+++ grails-app/domain/contactsdatabase/Contacts.groovy
@@ -2,6 +2,18 @@ package contactsdatabase
 
 class Contacts {
 
+    String firstName
+    String lastName
+    String email
+    String phoneNumber
+    Date dob
+
     static constraints = {
+
+        firstName (blank: false, matches: "[a-zA-Z]+")
+        lastName (blank: false, matches: "[a-zA-Z]+")
+        email (email: true, unique: true)
+        phoneNumber (blank: false, unique: true, matches: "[0-9]")
+        dob (blank: false)
     }
 }
