b0c0300e588539dac74f7b517edb75e546b67271
akshitagangrade <akshitagangrade2@gmail.com>
2017-05-22 23:27:41 +0530

    checking for already logged in user

--- grails-app/controllers/contactsdatabase/SignupController.groovy
+++ grails-app/controllers/contactsdatabase/SignupController.groovy
@@ -3,28 +3,34 @@ package contactsdatabase
 import com.contactsdatabase.Role
 import com.contactsdatabase.User
 import com.contactsdatabase.UserRole
+import grails.plugin.springsecurity.SpringSecurityService
 import grails.plugin.springsecurity.annotation.Secured
 
 class SignupController {
 
+    def springSecurityService
+
     @Secured('permitAll')
     def index() {
+
+        println(springSecurityService.isLoggedIn())
+        if (springSecurityService.isLoggedIn()) {
+            redirect(controller: 'Contacts', action: 'index')
+        }
+
         println "enters signup/index"
-        //[user: new Contacts()]
+        [user: new Contacts()]
     }
 
     @Secured('permitAll')
     def saving() {
 
         println params
-
         User userInstance = User.findByUsername("params.username")
 
         if (!userInstance) {
             userInstance = new User([username: params.username, password: params.password])
             userInstance.save()
-
-            println ">>>><<<"
             println params.errors
 
             if (userInstance.hasErrors()) {
@@ -33,15 +39,15 @@ class SignupController {
             }
             println "no errors"
 
-                Role roleAdmin = Role.findByAuthority('ROLE_ADMIN')
-                println roleAdmin
-                UserRole.create(userInstance, roleAdmin)
-                Role roleUser = Role.findByAuthority('ROLE_USER')
-                println roleUser
-                UserRole.create(userInstance, roleUser)
+            Role roleAdmin = Role.findByAuthority('ROLE_ADMIN')
+            println roleAdmin
+            UserRole.create(userInstance, roleAdmin)
+            Role roleUser = Role.findByAuthority('ROLE_USER')
+            println roleUser
+            UserRole.create(userInstance, roleUser)
 
-                flash.message = "Welcome!\nYour username is: ${userInstance.username}" + "\n please login to continue"
-                redirect(controller: 'login', action: 'auth')
+            flash.message = "Welcome!\nYour username is: ${userInstance.username}" + "\n please login to continue"
+            redirect(controller: 'login', action: 'auth')
             }
         }
     }
