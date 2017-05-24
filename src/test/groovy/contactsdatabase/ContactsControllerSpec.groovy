d6f75375e75a73e93dcfcf28fa1fce7cc458aa09
akshitagangrade <akshitagangrade2@gmail.com>
2017-05-24 23:55:20 +0530

    creating unit tests

--- src/test/groovy/contactsdatabase/ContactsControllerSpec.groovy
+++ src/test/groovy/contactsdatabase/ContactsControllerSpec.groovy
@@ -1,5 +1,8 @@
 package contactsdatabase
 
+import com.contactsdatabase.User
+import grails.plugin.springsecurity.SpringSecurityService
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,16 +10,25 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(ContactsController)
+@Mock([SpringSecurityService, Contacts2])
 class ContactsControllerSpec extends Specification {
+    void "test save"() {
+        given:
+        params.firstName = 'dem'
+        params.lastName = 'dem'
+        params.email = 'deo@gmail.com'
+        params.phoneNumber = '942521286'
+        params.markData = "Favourite"
+        params.date = "2017-03-03"
+        def userInstance = new User(username: "himani1@gmail.com", password: "hima04")
+        when:
+        params.dob = Date.parse("yyyy-MM-dd", params.date)
+        params.userInstance = userInstance
 
-    def setup() {
-    }
+        controller.save()
 
-    def cleanup() {
-    }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
+        then:
+        flash.message == "Contact details have been successfully saved!!!"
+        response.redirectUrl == '/contacts/list'
     }
 }
---
+++ src/test/groovy/contactsdatabase/SignupControllerSpec.groovy
@@ -0,0 +1,7 @@
+package contactsdatabase
+
+/**
+ * Created by dell1 on 24/5/17.
+ */
+class SignupControllerSpec {
+}
